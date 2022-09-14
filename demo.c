f61c2e5707340da9938c909e5a3468a067dcfa1c
unknown <alvindivano@gmail.com>
2022-09-14 23:54:08 +0700

    babi

---
+++ demo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int number;
+    printf("input angka: ");
+    scanf("%d",&number);
+    printf("\n");
+    if (number % 2 == 0){ 
+        printf("Number is even");
+    } else {
+        printf("Number is odd");
+    }
+    return 0;
+//return mengembalikan nilai dari sebuah fungsi
+//% sisa bagi *perkalian /pembagiaan
+//== tanda gae perbandingan
+
+
+}
